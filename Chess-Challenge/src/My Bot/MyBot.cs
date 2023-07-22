1966877e9c5f64c35822cdc2133f357ddab4f634
AspectOfTheNoob <Mcthouacbb@gmail.com>
2023-07-22 19:05:55 -0400

    Added time management

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -8,8 +8,10 @@ public class MyBot : IChessBot
 {
     private Move bestMove;
     private Board board;
+    private Timer timer;
 
-    private int ply;
+    private int ply, millisAlloced;
+    private bool shouldStop;
     //int nodes = 0;
 
     // TODO: Remove constants and replace them inline
@@ -17,74 +19,70 @@ public class MyBot : IChessBot
     // 0-5 = middlegame material, 6-11 = endgame material, 12-17 = phase weights
     short[] MAT_PHASE = new short[18];
 
-    /*{
-        70, 265, 291, 370, 794, 0
-    },
-        MATERIAL_EG = new int[]
-    {
-        101, 204, 225, 411, 749, 0
-    },
-        PHASES = new int[]
-    {
-        // pawn - queen
-        // extra zeroes are so that calculations can be packed into a single for loop with PSQT, rather than multiple loops
-        0, 6, 16, 18, 41, 0
-    };*/
-
     byte[] MVV_LVA = new byte[36];
-    ulong[] ARRAYS_INIT = new ulong[]
+	// these evaluation parameters were obtained from my engine, you can find them at https://github.com/mcthouacbb/Sirius
+    // The values are in the main branch under the Sirius/src/eval in material.h, phase.h, and psqt.cpp
+    // Additionally, the code for tuning the parameters is in the tune branch under Sirius/src/tune
+	ulong[] ARRAYS_INIT = new ulong[]
 	{
         // MVV_LVA
-		1733323143564695823, 3107790562467916834, 14683016086159413, 1443966850258900235, 3287070,
+		1733323143564695823, 3107790562467916834, 14683016086159413, 1443966850258900235,
+        // first 4 bytes is MVV_LVA, last 4 bytes is MAT_PHASE
+        74591169479321630,
+        // MAT_PHASE 2-17
+        3410228281635,
+        115687181809090661,
+        1688849860264685,
+        176094838800,
         // PSQT
-5565390404019549171,
-575335840744079079,
-17651010900099003871,
-17578661999434596061,
-1313345551101969536,
-2386923248285596926,
-868076469175061239,
-16997416005460222958,
-146097452330450155,
-2239131215378520820,
-363111507762086647,
-17073694706158276095,
-4629726883482379546,
-722250380910925559,
-17216393684550741214,
-141272842907741643,
-17441283546016189948,
-17581748350570205199,
-17940376040718661368,
-284271008960805096,
-2815597404505391877,
-15773275864558155004,
-13532727398902125497,
-18367956423823589898,
-5718570421002364646,
-16495828885755736624,
-16564494391039814121,
-16638239752875207143,
-645424268913470170,
-2169063745724152050,
-1443405841412131836,
-18229129728889977834,
-18230567993061078260,
-651061529587940357,
-866660323917759486,
-18298402315792217327,
-18084769465568460546,
-18304035169612267009,
-18229160596669136894,
-18302068091720104446,
-2959450934766862586,
-2887126245924599534,
-146341673907066359,
-15340074901198791398,
-18377793707869464285,
-1447363017716796675,
-1589210978859747066,
-16497497759637960425,
+        5565390404019549171,
+        575335840744079079,
+        17651010900099003871,
+        17578661999434596061,
+        1313345551101969536,
+        2386923248285596926,
+        868076469175061239,
+        16997416005460222958,
+        146097452330450155,
+        2239131215378520820,
+        363111507762086647,
+        17073694706158276095,
+        4629726883482379546,
+        722250380910925559,
+        17216393684550741214,
+        141272842907741643,
+        17441283546016189948,
+        17581748350570205199,
+        17940376040718661368,
+        284271008960805096,
+        2815597404505391877,
+        15773275864558155004,
+        13532727398902125497,
+        18367956423823589898,
+        5718570421002364646,
+        16495828885755736624,
+        16564494391039814121,
+        16638239752875207143,
+        645424268913470170,
+        2169063745724152050,
+        1443405841412131836,
+        18229129728889977834,
+        18230567993061078260,
+        651061529587940357,
+        866660323917759486,
+        18298402315792217327,
+        18084769465568460546,
+        18304035169612267009,
+        18229160596669136894,
+        18302068091720104446,
+        2959450934766862586,
+        2887126245924599534,
+        146341673907066359,
+        15340074901198791398,
+        18377793707869464285,
+        1447363017716796675,
+        1589210978859747066,
+        16497497759637960425,
 	};
 
     sbyte[] HALF_PSQT = new sbyte[384];
@@ -120,6 +118,7 @@ public class MyBot : IChessBot
             else if (moves[i] == killerMoves[ply, 0] || moves[i] == killerMoves[ply, 1])
                 score = -100;
 
+            // negate score to invert comparison and sort in descending order
             scores[i] = -score;
         }
 
@@ -128,8 +127,10 @@ public class MyBot : IChessBot
 
     public MyBot()
     {
+        // this is probably a hacky way to initialize the tables
         Buffer.BlockCopy(ARRAYS_INIT, 0, MVV_LVA, 0, 36);
-        Buffer.BlockCopy(ARRAYS_INIT, 40, HALF_PSQT, 0, 384);
+        Buffer.BlockCopy(ARRAYS_INIT, 36, MAT_PHASE, 0, 36);
+        Buffer.BlockCopy(ARRAYS_INIT, 72, HALF_PSQT, 0, 384);
         // dumb hack to get rid of braces, don't do this in real code
         for (int i = 0; i < 384; i += 4)
             for (int j = 0; j < 4; j++)
@@ -141,14 +142,25 @@ public class MyBot : IChessBot
 
     public Move Think(Board board, Timer timer)
     {
+        // use 2.5% of remaining time(decent strategy that doesn't take up many tokens)
+        millisAlloced = timer.MillisecondsRemaining / 40;
+        shouldStop = false;
         ply = 0;
         //nodes = 0;
 
 		this.board = board;
+        this.timer = timer;
 
-		Search(6, -200000, 200000, false);
-        //Console.WriteLine($"Nodes: {nodes}");
-        return bestMove;
+        // in case the search aborts on the first
+        Move move = board.GetLegalMoves()[0];
+        for (int i = 1; i < 128; i++)
+        {
+            Search(6, -200000, 200000, false);
+            if (shouldStop)
+                break;
+            move = bestMove;
+        }
+        return move;
     }
 
     int evaluate()
@@ -188,9 +200,14 @@ public class MyBot : IChessBot
     int Search(int depth, int alpha, int beta, bool doNull)
     {
         //nodes++;
+        if (timer.MillisecondsElapsedThisTurn > millisAlloced || shouldStop)
+        {
+            shouldStop = true;
+            return alpha;
+        }
 
         // if we already found a better mate, no need to search deeper
-		alpha = Math.Max(alpha, ply - 32000);
+        alpha = Math.Max(alpha, ply - 32000);
 		beta = Math.Min(beta, 32000 - ply);
 		if (alpha >= beta)
 			return alpha;
@@ -225,7 +242,6 @@ public class MyBot : IChessBot
             }
         }
 
-
         var moves = board.GetLegalMoves();
 
         // board.IsDraw() already detects stalemate, so no need to check for it again
@@ -241,6 +257,8 @@ public class MyBot : IChessBot
             ply++;
             // basic check extension
             int score = -Search(depth - (board.IsInCheck() ? 0 : 1), -beta, -alpha, true);
+            if (shouldStop)
+                return alpha;
             ply--;
             board.UndoMove(move);
 
@@ -267,6 +285,8 @@ public class MyBot : IChessBot
 
     int QSearch(int alpha, int beta)
     {
+        if (shouldStop)
+            return alpha;
         int eval = evaluate();
         if (eval >= beta)
             return beta;
